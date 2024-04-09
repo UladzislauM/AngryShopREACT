b8031886af065823219c1e5007859619f8fe1eeb
Marshalau Uladzislau <marshal25@yandex.by>
2024-04-09 22:08:31 +0300

    secound set

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Towel rails</span>
+            <ul className='nav'>
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Cabinet</li>
+            </ul>
+        </div>
+    <div className='presentation'></div>
+    </header>
+  )
+}
