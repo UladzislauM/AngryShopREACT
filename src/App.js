b34264847b7df4d334388b312e5195087be59b80
Uladzislau Marshalau <marshal25@yandex.by>
2024-04-10 22:20:28 +0300

    develop: implemented items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import React from "react"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
+import Items from "./components/Items";
+ 
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      items:[
+        {
+          id: 1,
+          title: 'Rostela Melodia 50x60',
+          img: 'rostela_melodia_50x60.jpeg',
+          descryption: 'Beautiful towel rail',
+          category: 'towel_rails',
+          price: '60.00'
+        },
+        {
+          id: 2,
+          title: 'Rostela Victoria 50x60',
+          img: 'rostela_victoria_50x60.jpeg',
+          descryption: 'Beautiful towel rail',
+          category: 'towel_rails',
+          price: '64.00'
+        }
+      ]
+    }
+  }
+    render(){
+      return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
   );
+ }
 }
 
 export default App;
