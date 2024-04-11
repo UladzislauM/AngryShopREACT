8006a20aaa1be20dd4ce4c348618a872f4efd901
Uladzislau Marshalau <marshal25@yandex.by>
2024-04-11 06:33:05 +0300

    develop: added cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { IconName } from "react-icons/fa6";
+import React, { useState } from 'react'
+import { FaBasketShopping } from "react-icons/fa6";
 
 export default function Header() {
+let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
         <div>
@@ -11,6 +13,12 @@ export default function Header() {
                 <li>Contacts</li>
                 <li>Cabinet</li>
             </ul>
+            <FaBasketShopping onClick={() => setCartOpen(cartOpen =!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+            {cartOpen && (
+              <div className='shop-cart'>
+              </div>
+            )}
         </div>
     <div className='presentation'></div>
     </header>
